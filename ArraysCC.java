86c4c0c599e3b683a2de5112b25b2ca9b9bfc5ab
Aditya Srivastava <aadi02anu07@gmail.com>
2025-08-31 22:37:49 +0530

    ArrayCC updated

--- ArraysCC.java
+++ ArraysCC.java
@@ -7,13 +7,32 @@ public class ArraysCC {
         }
     }
 
+    public static int linearSearch(int numbers[],int key){
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public static int MenuSearch(String menu[],String key){
+        for(int i=0;i<menu.length;i++){
+            if(menu[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         /*
         int marks[] = new int[50];
         int marks2[] = { 1, 2, 3 };
         int marks3[] = { 4, 5, 6 };
         String fruits[] = { "apple", "mango", "orange" };
-
+         
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your Physics marks:");
         marks[0] = sc.nextInt(); // Phy
@@ -21,24 +40,47 @@ public class ArraysCC {
         marks[1] = sc.nextInt(); // Chem
         System.out.print("Enter your Maths marks:");
         marks[2] = sc.nextInt(); // Math
-
+        
         System.out.println("Physics = " + marks[0]);
         System.out.println("Chem = " + marks[1]);
         System.out.println("Math = " + marks[2]);
-
+        
         marks[2] = marks[2] + 1; // we can also update array
         System.out.println("Math = " + marks[2]);
-
+        
         double percentage = ((marks[0] + marks[1] + marks[2]) / 300f) * 100f;
         System.out.println("Percentage= " + percentage + "%");
-
+         
         System.out.println("Array Length= " + marks.length);
-         */
-
+         
         int marks[] = { 97, 98, 99 };
         updateMarks(marks);
         for (int i = 0; i < marks.length; i++) {
-            System.out.println(marks[i] + " ");
+            System.out.println(marks[i]);
+        }
+
+        //Linear Search
+        int numbers[] = {2,4,6,8,10,12,14,16,18,20};
+        int key = 10;
+        
+        int index1=linearSearch(numbers, key);
+        if(index1==-1){
+            System.out.print("NOT FOUND");
+        }
+        else{
+            System.out.println("Found at index: "+index1);
+        }
+        */
+
+        //Linear Search for String type
+        String menu[]={"Dosha","Bholu Cholu","Idli","Sambar","Chatni"};
+        String key2="Bholu Cholu";
+        int index2 = MenuSearch(menu, key2);
+        if(index2==-1){
+            System.out.print("NOT FOUND");
+        }
+        else{
+            System.out.print("Item found at index: " + index2);
         }
     }
 }
