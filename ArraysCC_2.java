03d76b5d75eba8997fd2f18ecaf926c1a8354f32
Aditya Srivastava <aadi02anu07@gmail.com>
2025-10-12 09:25:57 +0530

    Kadans Added

--- ArraysCC_2.java
+++ ArraysCC_2.java
@@ -1,14 +1,13 @@
 public class ArraysCC_2 {
 
     public static void maxSubArraySum(int numbers[]) {
-        int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
 
         for (int i = 0; i < numbers.length; i++) {
             int start = i;
             for (int j = i; j < numbers.length; j++) {
                 int end = j;
-                currSum = 0;
+                int currSum = 0;
                 for (int k = start; k <= end; k++) {
                     currSum += numbers[k];
                 }
@@ -18,11 +17,48 @@ public class ArraysCC_2 {
                 }
             }
         }
-        System.out.println("Max Sum= " + maxSum);
+        System.out.println("Max Sum = " + maxSum);
+    }
+
+    public static void PrefixSubArraySum(int numbers[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+
+        prefix[0] = numbers[0];
+        // Calculate Prefix Array
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
+                int end = j;
+                int currSum = start == 0 ? prefix[end] : prefix[end] - (prefix[start - 1]);
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max Sum = " + maxSum);
+    }
+
+    public static void Kadans(int numbers[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            cs += numbers[i];
+            if (cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println("Max Subarray Sum is " + ms);
     }
 
     public static void main(String[] args) {
         int numbers[] = { 1, -2, 6, -1, 3 };
-        maxSubArraySum(numbers);
+        Kadans(numbers);
     }
 }
