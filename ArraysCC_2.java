03b06068d7c50bddae74cfd17ce4d10e24c4c68f
Aditya Srivastava <aadi02anu07@gmail.com>
2025-10-12 11:44:45 +0530

    RainWater done

--- ArraysCC_2.java
+++ ArraysCC_2.java
@@ -57,8 +57,39 @@ public class ArraysCC_2 {
         System.out.println("Max Subarray Sum is " + ms);
     }
 
+    public static int RainWater(int height[]) {
+        int n = height.length;
+
+        // calculate leftmax boundary - array
+        int leftmax[] = new int[n];
+        leftmax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftmax[i] = Math.max(height[i], leftmax[i - 1]);
+        }
+
+        // calculate rightmax boundary - array
+        int rightmax[] = new int[n];
+        rightmax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightmax[i] = Math.max(height[i], rightmax[i + 1]);
+        }
+
+        int trappedWater = 0;
+        // loop
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftmax[i], rightmax[i]);
+            trappedWater += waterLevel - height[i];
+        }
+
+        return trappedWater;
+    }
+
     public static void main(String[] args) {
+        /*
         int numbers[] = { 1, -2, 6, -1, 3 };
         Kadans(numbers);
+         */
+        int Height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        System.out.println(RainWater(Height));
     }
 }
