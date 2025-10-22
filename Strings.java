be1044d8a30faada12d6f8d25f90fa9c064f30be
Aditya Srivastava <aadi02anu07@gmail.com>
2025-10-22 17:01:10 +0530

    toUpperCase

--- Strings.java
+++ Strings.java
@@ -41,6 +41,24 @@ public class Strings {
         return (float) Math.sqrt(x2 + y2);
     }
 
+    public static String toUpperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String args[]) {
         char arr[] = { 'a', 'b', 'c', 'd' };
         // defining string
@@ -103,5 +121,8 @@ public class Strings {
             sb.append(ch);
         }
         System.out.println(sb);
+
+        String str6 = "hi, i am aditya";
+        System.out.println(toUpperCase(str6));
     }
 }
