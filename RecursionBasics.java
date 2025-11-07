af12f053cd523482de565e531e85eba5fe7c4ffc
Aditya Srivastava <aadi02anu07@gmail.com>
2025-11-08 02:19:25 +0530

    Recursion start

---
+++ RecursionBasics.java
@@ -0,0 +1,27 @@
+public class RecursionBasics {
+
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n - 1);
+    }
+
+    public static void printInc(int n) {
+        if (n == 1) {
+            System.out.print(n + " ");
+            return;
+        }
+        printInc(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String arr[]) {
+        int n = 10;
+        printDec(n);
+        System.out.println();
+        printInc(n);
+    }
+}
