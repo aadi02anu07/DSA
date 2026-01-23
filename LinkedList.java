435a0ac5be27ac7e7de7af1c72e56dc915d3b74d
Aditya Srivastava <aadi02anu07@gmail.com>
2026-01-23 19:54:19 +0530

    LinkedList begins

---
+++ LinkedList.java
@@ -0,0 +1,36 @@
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        // step 1 = create new node
+        Node newNode = new Node(data);
+
+        if (head == null) { // when there does NOT exist any node
+            head = tail = newNode;
+            return;
+        }
+
+        // step 2 - newNode next = head
+        newNode.next = head;// link
+
+        // step3 - head = newNode
+        head = newNode;
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+    }
+}
