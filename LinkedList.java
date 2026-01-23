f2b3bc6c9d425d3b05ffc1879c023d39dc62125f
Aditya Srivastava <aadi02anu07@gmail.com>
2026-01-23 22:06:48 +0530

    print LL added

--- LinkedList.java
+++ LinkedList.java
@@ -12,6 +12,7 @@ public class LinkedList {
     public static Node head;
     public static Node tail;
 
+    // addFirst - O(1)
     public void addFirst(int data) {
         // step 1 = create new node
         Node newNode = new Node(data);
@@ -21,16 +22,47 @@ public class LinkedList {
             return;
         }
 
-        // step 2 - newNode next = head
+        // step 2 - newNode next = old head
         newNode.next = head;// link
 
         // step3 - head = newNode
         head = newNode;
     }
 
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // printLL - O(n)
+    public void print() {
+        if (head == null) {
+            System.out.println("LinkedList is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
+        ll.print();
         ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
     }
 }
