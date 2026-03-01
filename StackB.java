b917bb9a0d10312dd28e94dc6d1e3c9e800a38f2
Aditya Srivastava <aadi02anu07@gmail.com>
2026-03-01 19:21:19 +0530

    Stack1 done

--- StackB.java
+++ StackB.java
@@ -86,6 +86,8 @@ public class StackB {
      * }
      * }
      */
+
+    // push at bottom
     public static void pushtAtBottom(Stack<Integer> s, int data) {
         // base case
         if (s.isEmpty()) {
@@ -99,16 +101,109 @@ public class StackB {
         s.push(top);
     }
 
-    public static void main(String[] args) {
-        // Stack s = new Stack();
-        Stack<Integer> s = new Stack<>(); // using java colllection framework
-        s.push(1);
-        s.push(2);
-        s.push(3);
+    public static String reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
 
-        pushtAtBottom(s, 4);
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+        }
+
+        return result.toString();
+    }
+
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverseStack(s);
+        pushtAtBottom(s, top);
+    }
+
+    public static void stockSpan(int stocks[], int span[]) {
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+
+        for (int i = 1; i < stocks.length; i++) {
+            int currPrice = stocks[i];
+            while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
+                s.pop();
+            }
+
+            if (s.isEmpty()) {
+                span[i] = i + 1;
+            } else {
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh;
+            }
+
+            s.push(i);
+        }
+    }
+
+    public static void printStack(Stack<Integer> s) {
         while (!s.isEmpty()) {
             System.out.println(s.pop());
         }
+    }
+
+    public static void main(String[] args) {
+        // Stack s = new Stack();
+        // Stack<Integer> s = new Stack<>(); // using java colllection framework
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+        // pushtAtBottom(s, 4);
+        // while (!s.isEmpty()) {
+        // System.out.println(s.pop());
+        // }
+
+        // String str = "HelloWorld";
+        // System.out.println(reverseString(str));
+
+        // reverseStack(s);
+        // printStack(s);
+
+        // int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
+        // int span[] = new int[stocks.length];
+        // stockSpan(stocks, span);
+
+        // for (int i = 0; i < span.length; i++) {
+        // System.out.println(span[i] + " ");
+        // }
+        int arr[] = { 6, 8, 0, 1, 3 };
+        Stack<Integer> s = new Stack<>();
+        int nextGreater[] = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            // while
+            while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+            // if-else
+            if (s.isEmpty()) {
+                nextGreater[i] = -1;
+            } else {
+                nextGreater[i] = arr[s.peek()];
+            }
+
+            // push in stack
+            s.push(i);
+        }
+
+        for (int i = 0; i < nextGreater.length; i++) {
+            System.out.print(nextGreater[i] + " ");
+        }
+        System.out.println();
     }
 }
