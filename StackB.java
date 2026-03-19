9427542bc090da364b3e26e5c6e3a2e0aa4f946a
Aditya Srivastava <aadi02anu07@gmail.com>
2026-03-20 01:45:33 +0530

    stack all done

--- StackB.java
+++ StackB.java
@@ -214,6 +214,51 @@ public class StackB {
         return false;
     }
 
+    public static void maxArea(int arr[]) {
+        int maxArea = 0;
+        int nsr[] = new int[arr.length];
+        int nsl[] = new int[arr.length];
+
+        // Next smaller right
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsr[i] = arr.length;
+            } else {
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // Next smaller left
+        s = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // current area:width=j-i-1=nsr[i]-nsl[i]-1
+        for (int i = 0; i < arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+        System.out.println("Maximum area in histogram = " + maxArea);
+    }
+
     public static void main(String[] args) {
         // Stack s = new Stack();
         // Stack<Integer> s = new Stack<>(); // using java colllection framework
@@ -265,9 +310,12 @@ public class StackB {
         // System.out.println();
 
         // String str = "({})[]";
-        String str1 = "((a+b))"; // true
-        String str2 = "(a-b)"; // false
-        System.out.println(isDuplicate(str1));
-        System.out.println(isDuplicate(str2));
+        // String str1 = "((a+b))"; // true
+        // String str2 = "(a-b)"; // false
+        // System.out.println(isDuplicate(str1));
+        // System.out.println(isDuplicate(str2));
+
+        int arr[] = { 2, 1, 5, 6, 2, 3 };// heights in histogram
+        maxArea(arr);
     }
 }
