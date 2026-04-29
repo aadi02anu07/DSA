40d8739e4399f7d4251b255614c5d4aedba28e59
Aditya Srivastava <aadi02anu07@gmail.com>
2026-04-29 16:11:02 +0530

    Activity selection done

---
+++ ClassroomGreedy.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class ClassroomGreedy {
+    public static void main(String args[]) {
+        // TC of this question is O(n log.n)
+        int start[] = { 1, 3, 0, 5, 8, 5 };
+        int end[] = { 2, 4, 6, 7, 9, 9 }; // already sorted on basis of end time
+
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+
+        // lambda function -> shortform
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2])); // O(logn)
+
+        int maxAct;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        // 1st activity
+        maxAct = 1;
+        ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
+        for (int i = 1; i < end.length; i++) { // O(n)
+            if (activities[i][1] >= lastEnd) {
+                // activity select
+                maxAct++;
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
+            }
+        }
+
+        System.out.println("Max activities = " + maxAct);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print("A" + ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
