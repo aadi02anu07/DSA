3be8ff2c781f14d41350e18c3e06ee51c298512c
Aditya Srivastava <aadi02anu07@gmail.com>
2026-05-12 00:16:43 +0530

    Completed job sequencing problem

--- ClassroomGreedy.java
+++ ClassroomGreedy.java
@@ -1,6 +1,19 @@
 import java.util.*;
 
 public class ClassroomGreedy {
+    static class Job {
+        int deadline;
+        int profit;
+        int id; // we are taking id as 0,1,2 instead of A,B,C...
+
+        public Job(int i, int d, int p) {
+            // making constructor
+            id = i;
+            deadline = d;
+            profit = p;
+        }
+    }
+
     public static void main(String args[]) {
         /*
          * // TC of this question is O(n log.n)
@@ -38,38 +51,121 @@ public class ClassroomGreedy {
          * System.out.print("A" + ans.get(i) + " ");
          * }
          * System.out.println();
+         * 
+         * int val[] = { 60, 100, 120 };
+         * int weight[] = { 10, 20, 30 };
+         * int W = 50;
+         * 
+         * double ratio[][] = new double[val.length][2];
+         * // 0th col => idx; 1st col => ratio;
+         * 
+         * for (int i = 0; i < val.length; i++) {
+         * ratio[i][0] = i;
+         * ratio[i][1] = val[i] / (double) weight[i];
+         * }
+         * 
+         * // ascending order sorting
+         * Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+         * 
+         * int capacity = W;
+         * int finalValue = 0;
+         * 
+         * // descending order sorting
+         * for (int i = ratio.length - 1; i >= 0; i--) {
+         * int idx = (int) ratio[i][0];
+         * if (capacity >= weight[idx]) { // include full item
+         * finalValue += val[idx];
+         * capacity -= weight[idx];
+         * } else { // include fractional item
+         * finalValue *= (ratio[i][1] * capacity);
+         * capacity = 0;
+         * break;
+         * }
+         * }
+         * System.out.println("Final Value = " + finalValue);
+         * 
+         * // O(nlogn)
+         * int A[] = { 4, 1, 8, 7 };
+         * int B[] = { 2, 3, 6, 5 };
+         * 
+         * Arrays.sort(A);
+         * Arrays.sort(B);
+         * 
+         * int minDiff = 0;
+         * 
+         * for (int i = 0; i < A.length; i++) {
+         * minDiff += Math.abs(A[i] - B[i]);
+         * }
+         * 
+         * System.out.println("Min absolute differnce of pair = " + minDiff);
+         * 
+         * // Max chain length
+         * // O(nlogn)
+         * int pairs[][] = { { 5, 24 }, { 39, 60 }, { 5, 28 }, { 27, 40 }, { 50, 90 } };
+         * 
+         * Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+         * 
+         * int chainLength = 1;
+         * int chainEnd = pairs[0][1]; // Last selected pair end //chain end
+         * 
+         * for (int i = 0; i < pairs.length; i++) {
+         * if (pairs[i][0] > chainEnd) {
+         * chainLength++;
+         * chainEnd = pairs[i][1];
+         * }
+         * }
+         * 
+         * System.out.println("Maximum length of chain = " + chainLength);
+         * 
+         * Integer coins[] = { 1, 2, 5, 10, 20, 50, 100, 500, 2000 };
+         * 
+         * Arrays.sort(coins, Comparator.reverseOrder());
+         * int countOfCoins = 0;
+         * int amount = 590;
+         * ArrayList<Integer> ans = new ArrayList<>();
+         * for (int i = 0; i < coins.length; i++) {
+         * if (coins[i] <= amount) {
+         * while (coins[i] <= amount) {
+         * countOfCoins++;
+         * ans.add(coins[i]);
+         * amount -= coins[i];
+         * }
+         * }
+         * }
+         * System.out.println("Total minimum coins used = " + countOfCoins);
+         * 
+         * for(int i=0;i<ans.size();i++){
+         * System.out.print(ans.get(i)+" ");
+         * }
+         * System.out.println();
          */
+        int jobsInfo[][] = { { 4, 20 }, { 1, 10 }, { 1, 40 }, { 1, 30 } };
 
-        int val[] = { 60, 100, 120 };
-        int weight[] = { 10, 20, 30 };
-        int W = 50;
+        ArrayList<Job> jobs = new ArrayList<>();
 
-        double ratio[][] = new double[val.length][2];
-        // 0th col => idx; 1st col => ratio;
-
-        for (int i = 0; i < val.length; i++) {
-            ratio[i][0] = i;
-            ratio[i][1] = val[i] / (double) weight[i];
+        for (int i = 0; i < jobsInfo.length; i++) {
+            jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
         }
 
-        // ascending order sorting
-        Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+        Collections.sort(jobs, (obj1, obj2) -> obj2.profit - obj1.profit);// "obj2.profit - obj1.profit" ->
+                                                                          // this will sort it in descending order of
+                                                                          // profit
 
-        int capacity = W;
-        int finalValue = 0;
-
-        // descending order sorting
-        for (int i = ratio.length - 1; i >= 0; i--) {
-            int idx = (int) ratio[i][0];
-            if (capacity >= weight[idx]) { // include full item
-                finalValue += val[idx];
-                capacity -= weight[idx];
-            } else { // include fractional item
-                finalValue *= (ratio[i][1] * capacity);
-                capacity = 0;
-                break;
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time = 0;
+        for (int i = 0; i < jobs.size(); i++) {
+            Job curr = jobs.get(i);
+            if (curr.deadline > time) {
+                seq.add(curr.id);
+                time++;
             }
         }
-        System.out.println("Final Value = " + finalValue);
+
+        // print seq
+        System.out.println("Max jobs = " + seq.size());
+        for (int i = 0; i < seq.size(); i++) {
+            System.out.print(seq.get(i) + " ");
+        }
+        System.out.println();
     }
 }
