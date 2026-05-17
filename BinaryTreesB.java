9dd9810bc46f40544faebab861f00ae459f2e232
Aditya Srivastava <aadi02anu07@gmail.com>
2026-05-17 15:17:08 +0530

    BT1 done

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.util.LinkedList;
 public class BinaryTreesB {
     static class Node {
         int data;
@@ -26,12 +28,71 @@ public class BinaryTreesB {
 
             return newNode;
         }
+
+        public void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        public void inorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        public void postorder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+
+        public void levelOrder(Node root){
+            if(root==null){
+                return;
+            }
+
+            Queue<Node>q=new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode=q.remove();
+                if(currNode==null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left!=null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right!=null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+
+        tree.levelOrder(root);
     }
 }
