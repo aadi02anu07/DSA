007033a0971d7d8c1182d4e2bee6d3d8bfb05a44
Aditya Srivastava <aadi02anu07@gmail.com>
2026-05-18 23:06:21 +0530

    BT2 done

--- ClassroomBinaryTree.java
+++ ClassroomBinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.util.LinkedList;
+
 public class ClassroomBinaryTree {
     static class Node {
         int data;
@@ -40,6 +43,122 @@ public class ClassroomBinaryTree {
         return leftsum + rightsum + root.data;
     }
 
+    public static int diameter(Node root) {// O(n^2)
+        if (root == null) {
+            return 0;
+        }
+
+        int ldiam = diameter(root.left);
+        int rdiam = diameter(root.right);
+        int lh = height(root.left);
+        int rh = height(root.right);
+        int selfDiam = lh + rh + 1;
+
+        return Math.max(selfDiam, Math.max(ldiam, rdiam));
+    }
+
+    static class Info {
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter2(Node root) { // O(n)
+        if (root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter2(root.left);
+        Info rightInfo = diameter2(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
+    }
+
+    public static boolean isIdentical(Node node, Node subRoot) {
+        if (node == null && subRoot == null) {
+            return true;
+        } else if (node == null || subRoot == null || node.data != subRoot.data) {
+            return false;
+        }
+
+        if (!isIdentical(node.left, subRoot.left)) {
+            return false;
+        }
+        if (!isIdentical(node.right, subRoot.right)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == subRoot.data) {
+            if (isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
+    }
+
+    static class Info1 {
+        Node node;
+        int hd; // horizontal distance
+
+        public Info1(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public static void topView(Node root) {
+        // Level Order Traversal
+        Queue<Info1> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+
+        int min = 0, max = 0;
+        q.add(new Info1(root, 0));
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Info1 curr = q.remove();
+            if (curr == null) {
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if (!map.containsKey(curr.hd)) { // first time my hd(Horizontal distance) is occuring
+                    map.put(curr.hd, curr.node);
+                }
+
+                if (curr.node.left != null) {
+                    q.add(new Info1(curr.node.left, curr.hd - 1));
+                    min = Math.min(min, curr.hd - 1);
+                }
+                if (curr.node.right != null) {
+                    q.add(new Info1(curr.node.right, curr.hd + 1));
+                    max = Math.max(max, curr.hd + 1);
+                }
+            }
+        }
+
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i).data + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String args[]) {
         /*
          * 1
@@ -56,8 +175,24 @@ public class ClassroomBinaryTree {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        System.out.println("Height of tree = " + height(root));
-        System.out.println("Count of tree = " + count(root));
-        System.out.println("Sum of tree = " + sum(root));
+        /*
+         * 2
+         * / \
+         * 4 5
+         */
+
+        // System.out.println("Height of tree = " + height(root));
+        // System.out.println("Height of tree = " + diameter2(root).ht);
+        // System.out.println("Count of tree = " + count(root));
+        // System.out.println("Sum of tree = " + sum(root));
+        // System.out.println("Diameter of tree = " + diameter2(root).diam);
+
+        Node subRoot = new Node(2);
+        subRoot.left = new Node(4);
+        subRoot.right = new Node(5);
+
+        // System.out.println(isSubtree(root, subRoot));
+
+        topView(root);
     }
 }
