b060c0a862be86da3d079363a460ca1f94412158
Aditya Srivastava <aadi02anu07@gmail.com>
2026-07-26 11:31:06 +0530

    Weakest soldier

--- heapSort.java
+++ heapSort.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class heapSort {
     public static void heapify(int arr[], int i, int size) {
         int left = 2 * i + 1;
@@ -40,14 +42,110 @@ public class heapSort {
         }
     }
 
-    public static void main(String[] args) {
-        int arr[] = { 1, 2, 4, 5, 3 };
-        heapSort(arr);
+    // Nearby cars question of heapify section using Priority Queue
+    static class Point implements Comparable<Point> {
+        int x;
+        int y;
+        int distSq;
+        int idx;
 
-        // print
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        public Point(int x, int y, int distSq, int idx) {
+            this.x = x;
+            this.y = y;
+            this.distSq = distSq;
+            this.idx = idx;
         }
-        System.out.println();
+
+        @Override
+        public int compareTo(Point p2) {
+            return this.distSq - p2.distSq; // ascending order sorting
+        }
+    }
+
+    // Weakest Soldier
+    static class Row implements Comparable<Row> {
+        int soldiers;
+        int idx;
+
+        public Row(int soldier, int idx) {
+            this.soldiers = soldier;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Row r2) {
+            if (this.soldiers == r2.soldiers) {
+                return this.idx - r2.idx;
+            } else {
+                return this.soldiers - r2.soldiers;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /*
+         * int arr[] = { 1, 2, 4, 5, 3 };
+         * heapSort(arr);
+         * 
+         * // print
+         * for (int i = 0; i < arr.length; i++) {
+         * System.out.print(arr[i] + " ");
+         * }
+         * System.out.println();
+         * System.out.println(
+         * "********************************************************************************************"
+         * );
+         * // Nearby cars question of heapify section using Priority Queue
+         * int pts[][] = { { 3, 3 }, { 5, -1 }, { -2, 4 } };
+         * int k = 2;
+         * 
+         * PriorityQueue<Point> pq = new PriorityQueue<>();
+         * for (int i = 0; i < pts.length; i++) {
+         * int distSq = pts[i][0] * pts[i][0] + pts[i][1] * pts[i][1];
+         * pq.add(new Point(pts[i][0], pts[i][1], distSq, i));
+         * }
+         * 
+         * // nearest K cars
+         * for (int i = 0; i < k; i++) {
+         * System.out.println("C" + pq.remove().idx);
+         * }
+         * 
+         * // Connect N ropes with Minimum cost
+         * int ropes[] = { 2, 3, 3, 4, 6 };
+         * 
+         * PriorityQueue<Integer> pq = new PriorityQueue<>();
+         * for (int i = 0; i < ropes.length; i++) {
+         * pq.add(ropes[i]);
+         * }
+         * 
+         * int cost = 0;
+         * while (pq.size() > 1) {
+         * int min = pq.remove();
+         * int min2 = pq.remove();
+         * cost += min + min2;
+         * pq.add(min + min2);
+         * }
+         * 
+         * System.out.println("Cost of connectiong N ropes = " + cost);
+         */
+
+        // Weakest Soldier
+        int army[][] = { { 1, 0, 0, 0 },
+                { 1, 1, 1, 1 },
+                { 1, 0, 0, 0 },
+                { 1, 0, 0, 0 } };
+        int k = 2;
+        PriorityQueue<Row> pq = new PriorityQueue<>();
+        for (int i = 0; i < army.length; i++) {
+            int count = 0;
+            for (int j = 0; j < army[0].length; j++) {
+                count += army[i][j] == 1 ? 1 : 0;
+            }
+            pq.add(new Row(count, i));
+        }
+
+        for (int i = 0; i < k; i++) {
+            System.out.println("R" + pq.remove().idx);
+        }
     }
 }
